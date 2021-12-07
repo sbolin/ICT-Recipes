6e4755e8cb9972a028204b5181173cf85da3546d
Scott Bolin <sbolin@gmail.com>
2021-12-08 02:05:15 +0700

    Initial Commit

---
+++ ICT Recipes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ICT Recipes
+//
+//  Created by Scott Bolin on 8-Dec-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ICT Recipes/ICT_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICT_RecipesApp.swift
+//  ICT Recipes
+//
+//  Created by Scott Bolin on 8-Dec-21.
+//
+
+import SwiftUI
+
+@main
+struct ICT_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
